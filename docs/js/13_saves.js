8614f7673cbc1fcc3f1e1be885a60b3b34c7b1ab
Aether941 <63092392+Aether941@users.noreply.github.com>
2026-09-13 09:09:16 +0800

    v0.14.4: 更新导出存档JSON生成的文件名，替换存档

--- docs/js/13_saves.js
+++ docs/js/13_saves.js
@@ -257,7 +257,7 @@ function exportSave() {
   const stamp = `${now.getFullYear()}${p(now.getMonth() + 1)}${p(now.getDate())}${p(now.getHours())}${p(now.getMinutes())}${p(now.getSeconds())}`;
   const carry = globalThis.game.num_gems;
   const wand = globalThis.game.has_wand ? "wand" : "nowand";
-  a.download = `promesst2-slot${activeSlot}-gems${carry}-${wand}-${stamp}.json`;
+  a.download = `promesst2web-gems${carry}-${wand}-${stamp}.json`;
   a.click();
   setTimeout(function () {
     URL.revokeObjectURL(a.href);
